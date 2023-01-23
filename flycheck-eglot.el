90e7bb137e5484f74bba7e38e15d22b41e205c51
Sergey V. Firsov <intramurz@gmail.com>
2023-01-24 01:30:30 +0300

    Add package metadata header

--- flycheck-eglot.el
+++ flycheck-eglot.el
@@ -1,5 +1,33 @@
-;;; flycheck-eglot --- flycheck support for eglot -*- lexical-binding: t -*-
+;;; flycheck-eglot.el --- Flycheck support for eglot -*- lexical-binding: t -*-
+
+;; Author: Sergey Firsov <intramurz@gmail.com>
+;; Maintainer: Sergey Firsov <intramurz@gmail.com>
+;; Version: 0.1
+;; Package-Requires: ((emacs "28.1"))
+;; URL: https://github.com/intramurz/flycheck-eglot
+;; Keywords: tools flycheck eglot
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 ;;; Commentary:
+
+;; commentary
+
 ;;; Code:
 
 
